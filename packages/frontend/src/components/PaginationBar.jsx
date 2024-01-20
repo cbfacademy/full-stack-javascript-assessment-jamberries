15795efd827e2df8b5c83442fc4f523b142a1f98
Jasmine S Beresford <jasminesberesford@gmail.com>
2024-01-20 00:22:29 +0000

    007:Update UI

--- packages/frontend/src/components/PaginationBar.jsx
+++ packages/frontend/src/components/PaginationBar.jsx
@@ -1,36 +1,33 @@
-import Pagination from 'react-bootstrap/Pagination';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 const PaginationBar = (props) => {
    
     // Create an array containing the numbers of pages
-    const pagesArray = new Array(props.pages).fill(null).map((number,index) => index)
+    //const pagesArray = new Array(props.pages).fill(null).map((number,index) => index)
 
     //SET PAGE NUMBER
-
-    const goToPreviousPage = () => {
-        props.setPageNumber(Math.max(0, props.pageNumber -1));
-    }
-
-    const goToNextPage = () => {
-        props.setPageNumber(Math.min(props.pages -1, props.pageNumber +1));
-    };
     
-    function handlePageChange(event) {
-        event.preventDefault()
-        props.setPageNumber(event.target.innerText - 1)
+    function handlePageChange(event, value) {
+        props.setPageNumber(value)
     }
 
-  return (
-     <Pagination className='pagBar'>
-         <Pagination.Prev onClick={goToPreviousPage}/>
-        {
-            pagesArray.map((pageIndex) => (
-                <Pagination.Item key={pageIndex} active={props.pageNumber === pageIndex}  onClick={handlePageChange}> {pageIndex+1} </Pagination.Item>
-            ))
-        }
-        <Pagination.Next onClick={goToNextPage}/>
-    </Pagination>
-  );
+//   return (
+//      <Pagination className='pagBar'>
+//          <Pagination.Prev onClick={goToPreviousPage}/>
+//         {
+//             pagesArray.map((pageIndex) => (
+//                 <Pagination.Item key={pageIndex} active={props.pageNumber === pageIndex}  onClick={handlePageChange}> {pageIndex+1} </Pagination.Item>
+//             ))
+//         }
+//         <Pagination.Next onClick={goToNextPage}/>
+//     </Pagination>
+//   );
+    return (
+        <Stack spacing={2}>
+            <Pagination color="secondary" count={props.pages} onChange={handlePageChange} />
+        </Stack>
+    );
 }
 
 export default PaginationBar;
