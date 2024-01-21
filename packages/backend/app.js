db91fa56c7e1948c9015f5af675403dc11e43865
Jasmine S Beresford <jasminesberesford@gmail.com>
2024-01-21 03:49:03 +0000

    008:Fix bug

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -5,7 +5,10 @@ const path = require('path');
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
-const routes = require("./routes")
+const Films = require('./models/FilmsModel.js');
+const Genre = require("./models/GenreModel");
+const Films = require('./models/FilmsModel.js');
+const lib = require('./lib/lib')
 const uri = process.env.MONGO_URI; 
 
 const app = express();
@@ -20,7 +23,6 @@ const PORT = process.env.PORT || 8000;
 mongoose.connect(uri)
 .then(() => {
   const app = express()
-  app.use("/api", routes) 
 
   app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
@@ -29,33 +31,105 @@ mongoose.connect(uri)
 })
 
 
-app.use(bodyParser.json({ limit: '10mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
+app.use(bodyParser.json({ }))
+app.use(bodyParser.urlencoded({ extended: false }))
 //app.use(express.static(path.join(__dirname, '../frontend/public')))
 
   //client.close();
 // Specify all of the routes
 
-app.use('/', require('./routes/index'))
-app.use('/api/home-page', require('./routes/index'))
-app.use('/api/films', require('./routes/films'))
-app.use('/api/genres', require('./routes/films'))
-app.use('/api/actors', require ('./routes/actors'))
+app.get('/api/home-page', async (req, res) => {
+  const query =  {actor_count : {$gt : 1}}
+  const pageSize = 7;
+  const films = await Films.find(query)
+  .limit(pageSize)
+  res.json(films);
+})
 
 
+app.get('/api/films/:id', async (req, res) => {
+  const tmdb_id = parseInt(req.params.id)
+  const film = await Films.findOne({tmdb_id : tmdb_id}).exec();
+  res.json(film);
+})
 
-  
-  // error handler
-  app.use(function (err, req, res, next) {
-    if (err.name === 'NotFoundError') {
-      res.status(404)
-    } else if (err.name === 'ForbiddenError') {
-      res.status(403)
-    } else {
-      console.log(err)
-      res.status(500)
+app.get('/api/genres', async (req, res) => {
+  const genres = await Genre.find({})
+  .sort({name: 'asc'})
+  res.json(genres);
+})
+
+app.get('/api/films', async (req, res) => {
+  const page = parseInt(req.query.page || "0")
+//  const query = !req.query.genre ? {actor_count : {$gt : 1}} : {actor_count : {$gt : 1}, genres : parseInt(req.query.genre)}
+  const query = !req.query.genre ? {} :{ genres : parseInt(req.query.genre)}
+  const pageSize = 18;
+  const totalFilms = await Films.countDocuments({})
+  const films = await Films.find(query)
+  .sort({title: 'asc'})
+  .limit(pageSize)
+  .skip(pageSize * page);
+
+  res.json({
+     films, 
+     pages: Math.ceil(totalFilms / pageSize)});
+})
+
+app.post('/api/films', async (req, res) => {
+  const array = req.body.map( item => item.id)
+  const actorIdArray= array.map(item => {
+     return `${process.env.TMDB_ACTOR_CREDITS_URL}${item}/movie_credits?api_key=${process.env.TMDB_KEY}`
+ })
+ lib.databaseFunction(actorIdArray)
+ res.send('success')
+  console.error(error)
+  res.status(500).send("Server Error")
+})
+
+
+app.get('/api/actors', async (req, res) => {
+  const page = parseInt(req.query.page || "0")
+  const pageSize = 18;
+  const totalActors = await Actors.countDocuments({})
+  const actors = await Actors.find({})
+  .sort({name: 'asc'})
+  .limit(pageSize)
+  .skip(pageSize * page);
+
+  res.json({
+    actors, 
+    pages: Math.ceil(totalActors / pageSize)});
+  console.error(error)
+  res.status(500).send("Server Error")
+})
+
+app.post('/api/actors', async (req, res) => {
+try {
+  const array = req.body.map( item => {
+     const { id: tmdb_id, ...rest } = item;
+     return { tmdb_id, ...rest }
     }
-    res.send()
-  })
+   );
+
+  const actors = await Actors.insertMany(array)
+  .then(function () {
+     res.send({data:'Success'})
+     console.log("Data inserted") 
+ }).catch(function (error) {
+     console.log(error)     
+     res.send({data:'Success'})
+ });
+
+ 
+
+} catch (error) {
+  console.error(error)
+  res.status(500).send("Server Error")
+}
+})
+
+
+
+
 
 module.exports = app
--- packages/backend/routes/index.js
+++ packages/backend/routes/index.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const router = express.Router()
-const Films = require('../models/FilmsModel.js');
+// const express = require("express");
+// const router = express.Router()
+// const Films = require('../models/FilmsModel.js');
 
-router.get('/api/home-page', async (req, res) => {
-     const query =  {actor_count : {$gt : 1}}
-     const pageSize = 7;
-     const films = await Films.find(query)
-     .limit(pageSize)
-     res.json(films);
-})
+// router.get('/api/home-page', async (req, res) => {
+//      const query =  {actor_count : {$gt : 1}}
+//      const pageSize = 7;
+//      const films = await Films.find(query)
+//      .limit(pageSize)
+//      res.json(films);
+// })
 
 
-module.exports = router
+// module.exports = router
